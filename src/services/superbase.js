3886601bf6dd25141f786316e8ffcce46293c468
SsimbwaJoshua <ssimbwajoshua124@gmail.com>
2023-10-19 11:05:48 +0300

    setting up supabase

---
+++ src/services/superbase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://dhzumtcqlyktwmpbioub.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRoenVtdGNxbHlrdHdtcGJpb3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc2OTU2MTYsImV4cCI6MjAxMzI3MTYxNn0.UJqN3Scj87P3jpP5O89eMkjzJ5_0jF7KBndqJSfubKE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
